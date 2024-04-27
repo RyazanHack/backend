4c12f6e6bdb8336a7cc0e590ef57851dcf603f7d
Kripipastt <ivan.voronin.25@mail.ru>
2024-04-27 16:17:25 +0300

    Bug fix

--- vote/routers.py
+++ vote/routers.py
@@ -4,14 +4,14 @@ from random import randint
 
 from users.models import User
 
-votes_router = APIRouter(tags=["votes"], prefix="/votes")
+vote_router = APIRouter(tags=["votes"], prefix="/votes")
 
 
-@votes_router.post('/upvote')
-def upvote(user: User):
+@vote_router.post('/upvote')
+def upvote(user):
     return {'success': 'ok'}
 
 
-@votes_router.get('/{region_name}')
+@vote_router.get('/{region_name}')
 def get_all_votes(region_name: str):
     return {'region_name': region_name, 'amount': randint(1, 4445444)}
