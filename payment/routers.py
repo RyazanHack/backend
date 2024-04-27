e23c648c9fca447d8fbfffb989308fd278d1975e
Kripipastt <ivan.voronin.25@mail.ru>
2024-04-27 16:55:36 +0300

    Bug fix

--- payment/routers.py
+++ payment/routers.py
@@ -1,9 +1,12 @@
-from fastapi import APIRouter
+from typing import Annotated
+
+from fastapi import APIRouter, Depends
 from users.models import User
+from users.services import UserService
 
 payment_router = APIRouter(tags=["payment"], prefix="/payment")
 
 
 @payment_router.post("/{count_vote}")
-async def payment(user: User, count_vote: int):
+async def payment(current_user: Annotated[User, Depends(UserService().get_current_user)], count_vote: int):
     return {'url_for_pay': 'http://localhost:80'}
--- vote/routers.py
+++ vote/routers.py
@@ -1,17 +1,20 @@
-from fastapi import APIRouter, Header
+from typing import Annotated
+
+from fastapi import APIRouter, Header, Depends
 
 from random import randint
 
 from users.models import User
+from users.services import UserService
 
 vote_router = APIRouter(tags=["votes"], prefix="/votes")
 
 
-@vote_router.post('/upvote')
-def upvote(user):
+@vote_router.post('/upvote/{count_vote}')
+def upvote(current_user: Annotated[User, Depends(UserService().get_current_user)], count_vote: int):
     return {'success': 'ok'}
 
 
 @vote_router.get('/{region_name}')
-def get_all_votes(region_name: str):
+def get_all_votes(current_user: Annotated[User, Depends(UserService().get_current_user)], region_name: str):
     return {'region_name': region_name, 'amount': randint(1, 4445444)}
