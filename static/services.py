506362b1878d913c39556ae48bae2d809e76f134
VinGP <105109338+VinGP@users.noreply.github.com>
2024-04-28 06:50:29 +0300

    add xlsx

--- static/services.py
+++ static/services.py
@@ -1,9 +1,15 @@
+import pathlib
+import uuid
+from copy import deepcopy
+
 from config import ALL_REGIONS
 from payment.repositories import PaymentRepository
+from stages.services import StageService
 from users.repositories import UserRepository
 from vote.models import Vote
 from vote.repositories import VoteRepository
 from .schemas import UsersRegisteredResponse, VotesPurchasedResponse
+import xlsxwriter
 
 
 class StatisticService:
@@ -51,3 +57,38 @@ class StatisticService:
             p = round(region[1] / total_votes * 100, 2)
             res.append((region[0], region[1], p))
         return res[:10]
+
+    async def get_xlsx_file(self):
+        filename = str(uuid.uuid4())
+        filepath = f'./tmp/{filename}.xlsx'
+        workbook = xlsxwriter.Workbook(filepath)
+        worksheet = workbook.add_worksheet()
+        regions = {region: {_region: 0 for _region in ALL_REGIONS.keys()} for region in
+                   ALL_REGIONS.keys()}
+        for v in await Vote.objects.filter(
+                stage=(await StageService().get_current_stage())).all():
+            to_region = v.region
+            from_region = (await UserRepository().get_by_id(v.user_id)).region
+            regions[to_region][from_region] += v.amount
+
+        bold = workbook.add_format({'bold': True})
+
+        i = 0
+
+        for region in ALL_REGIONS.keys():
+            total = 0
+            worksheet.write(i, 0, region, bold)
+            i += 1
+            for _region in ALL_REGIONS.keys():
+                if regions[region][_region] == 0:
+                    continue
+                total += regions[region][_region]
+                worksheet.write(i, 0, _region)
+                worksheet.write(i, 1, regions[region][_region])
+                i += 1
+            worksheet.write(i, 0, "Итого", bold)
+            worksheet.write(i, 1, total)
+            i += 1
+
+        workbook.close()
+        return filepath
